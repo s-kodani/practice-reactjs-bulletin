ea50ff9ee397d3e573edd2a51754b1f389cd922e
Shinnosuke Kodani <kocchi.jan12@gmail.com>
2023-01-04 17:55:37 +0900

    Update API base url.

--- src/common/const.ts
+++ src/common/const.ts
@@ -1,3 +1,5 @@
 export const API_CONFIG = {
-  BASE_URL: 'https://railway-react-bulletin-board.herokuapp.com'
+  BASE_URL: 'https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com',
+  MUX_FETCH_POSTS: 10,
+  MAX_FETCH_THREADS: 10
 } as const;
